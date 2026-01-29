ba09dbc9790e3a727c9024c24aaa53286ed720fc
Saadbinzahid1 <sbinzahid2@gmail.com>
2026-01-29 17:11:34 +0500

    Lets start

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
+
+// https://restcountries.com/v2/name/portugal
+
+// https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
+
+///////////////////////////////////////
